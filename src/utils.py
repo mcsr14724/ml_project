cd1a1683a4969ddcfb1ed0d295cae365aa182beb
mcsr14724 <chandumaddasani145@gmail.com>
2026-02-24 19:36:32 +0530

    Hyper-parameter tuning is added in evaluate model function

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_obj(file_path,obj):
     try:
@@ -20,22 +21,38 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
             model_name=list(models.keys())[i]
             model=list(models.values())[i]
-            model.fit(x_train,y_train)
+            param=params[model_name]
 
-            y_train_pred=model.predict(x_train)
-            y_test_pred=model.predict(x_test)
+            if param:
+                randomcv=RandomizedSearchCV(
+                    estimator=model,
+                    param_distributions=param,
+                    scoring='r2',
+                    n_iter=50,
+                    n_jobs=-1,
+                    cv=5,
+                    verbose=0,
+                    random_state=1
+                )
+                randomcv.fit(x_train,y_train)
+                y_train_pred=randomcv.predict(x_train)
+                y_test_pred=randomcv.predict(x_test)
+            else:
+                model.fit(x_train,y_train)
+                y_train_pred=model.predict(x_train)
+                y_test_pred=model.predict(x_test)
 
             train_score=r2_score(y_train,y_train_pred)
             test_score=r2_score(y_test,y_test_pred)
 
             report[model_name]=test_score
 
-            return report
+        return report
     except Exception as e:
         raise CustomException(e,sys)
