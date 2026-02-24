8172cc93fbafae11355720571be117d2c8dee155
mcsr14724 <chandumaddasani145@gmail.com>
2026-02-24 21:17:48 +0530

    trained models are added to return

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -155,13 +155,12 @@ class ModelTrainer:
                 }
             }
 
-            model_report:dict=evaluate_model(x_train,y_train,x_test,y_test,models,params)
-
+            model_report,trained_models = evaluate_model(x_train, y_train, x_test, y_test, models, params)
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            best_model=models[best_model_name]
+            best_model=trained_models[best_model_name]
 
             if best_model_score<0.6:
                 raise CustomException('no best model is found')
