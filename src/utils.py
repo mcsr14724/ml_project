a3fccaaaff907a6acc2ac866e88e6afe2474a056
mcsr14724 <chandumaddasani145@gmail.com>
2026-02-24 15:40:39 +0530

    evaluate_model function is implemented

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.logger import logging
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 def save_obj(file_path,obj):
     try:
@@ -16,5 +17,25 @@ def save_obj(file_path,obj):
         with open(file_path,'wb') as f:
             dill.dump(obj,f)  
         logging.info('dumping is completed')     
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model_name=list(models.keys())[i]
+            model=list(models.values())[i]
+            model.fit(x_train,y_train)
+
+            y_train_pred=model.predict(x_train)
+            y_test_pred=model.predict(x_test)
+
+            train_score=r2_score(y_train,y_train_pred)
+            test_score=r2_score(y_test,y_test_pred)
+
+            report[model_name]=test_score
+
+            return report
     except Exception as e:
         raise CustomException(e,sys)
